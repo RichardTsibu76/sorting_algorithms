2555b58b8a7cfffb0799c3ac595db2a8ab8c8665
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-01-22 12:01:44 +0100

    task o

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,43 +1,43 @@
-#include "sort.h"
-
 /**
- * swap - A swapping function sorting algorithm
- * @a: First number at consideration to swapped
- * @b: The second number
+ * swap - This is the swap function
+ * @a: First integer to be swapped
+ * @b: The second integer to be swapped
  */
+
 
 void swap(int *a, int *b)
 {
-	int keep;
+	int hold;
 
-	keep = *a;
+	hold = *a;
 	*a = *b;
-	*b = keep;
+	*b = hold;
 }
 
 /**
- * bubble_sort - This function sorts an array of integers
- *     in ascending order using Bubble sort algorithm
- * @array: Array of elements of the algorithm
- * @size: Array Size of the elements
+ * bubble_sort - A function to sort elements using the bubble sort method
+ *
+ * @array: Array of elements
+ *
+ * @size: Size of the array
  */
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t num, K;
+	size_t i, j;
 
 	if (array == NULL)
 		return;
-	/* Control flow of the passes */
-	for (num = 1; num <= size; num++)
+	/* Loop to control the number of passes */
+	for (i = 1; i <= size; i++)
 	{
-		/* This loop is to compare the number of pasess */
-		for (K = 0; K < size - 1; K++)
+		/* Loop to control the number of comparisons per pass */
+		for (j = 0; j < size - 1; j++)
 		{
-			/* Leverages the swapping if currrent element> next */
-			if (array[K] > array[K + 1])
+			/* Swap if current element > next */
+			if (array[j] > array[j + 1])
 			{
-				swap(&array[K], &array[K + 1]);
+				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
 			}
 		}
