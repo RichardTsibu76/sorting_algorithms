1923939b2a38c755a3f990aaeef3497d26a276e5
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-01-24 01:10:06 +0100

    100-shell_sort.c is updated

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,44 +1,43 @@
 #include "sort.h"
 
 /**
- * swap - swaps the value of two integers
- * @a: int
- * @b: int
+ * swap - This swapping about two integers
+ * @a: int one
+ * @b: int two
  */
 void swap(int *a, int *b)
 {
-	int hold;
+	int keep;
 
-	hold = *a;
+	keep = *a;
 	*a = *b;
-	*b = hold;
+	*b = keep;
 }
 
 /**
- * shell_sort - implements the shell short alogrithm using
- *	knuth sequence [1, 4, 13, 40, ...]
- *
- * @array: the array to be sorted
- * @size: the size of array
+ * shell_sort - This funtion sorts an array of integers in
+ *	ascending order
+ * @array: The array to be sorted
+ * @size: Array size
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, i, j;
+	size_t space, n, p;
 
 	if (array == NULL || size < 2)
 		return;
 
-	gap = 1;
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
-	while (gap >= 1)
+	space = 1;
+	while (space < size / 3)
+		space = space * 3 + 1;
+	while (space >= 1)
 	{
-		for (i = gap; i < size; i++)
+		for (n = space; n < size; n++)
 		{
-			for (j = i; j >= gap && (array[j] < array[j - gap]); j -= gap)
-				swap(&array[j], &array[j - gap]);
+			for (p = n; p >= space && (array[p] < array[p - space]); p -= space)
+				swap(&array[p], &array[p - space]);
 		}
-		gap /= 3;
+		space /= 3;
 		print_array(array, size);
 	}
 }
