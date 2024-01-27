b22c700443020ce32e948347e369ef13434c3bdb
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-01-27 16:14:02 +0100

    updated header files

--- sort.h
+++ sort.h
@@ -1,28 +1,23 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <stddef.h>
 
 /**
- * struct listint_s - Doubly linked list node
+ *struct listint_s - Doubly linked list node
  *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
+ *@n: Integer stored in the node
+ *@prev: Pointer to the previous element of the list
+ *@next: Pointer to the next element of the list
  */
-
-/*This is the user defined datatype for the project*/
-
 typedef struct listint_s
 {
-	const int n;
-	struct
-		listint_s *next;
+		const int n;
+			struct listint_s *prev;
+				struct listint_s *next;
 } listint_t;
-
-/*These are the functions prototypes for the files*/
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *head);
@@ -42,5 +37,4 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-
-#endif /* SORT_H */
+#endif
